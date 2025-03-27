4c48babeebf3e180a961b106060710a9aa925ccd
psp-birt <95621146+psp-birt@users.noreply.github.com>
2025-03-27 21:50:16 +0100

    Add files via upload

---
+++ PSP04_HTTP_WebClient_MostrarPágina/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Text;
+
+
+namespace HTTP
+{
+    class Program
+    {
+        public static void Main(String[] args)
+        {
+
+            // Creamos el objeto cliente
+
+            WebClient client = new WebClient();
+                      
+            //Se abre la página y se guarda en objeto stream
+            Stream data = client.OpenRead("https://www.google.com");
+            //Se prepara para la lectura
+            StreamReader lectura = new StreamReader(data);
+            //Se lee el contenido
+            string resultado = lectura.ReadToEnd();
+            //Muestra el resultado
+            Console.WriteLine(resultado);
+
+            //Cerramos los objetos de tipo stream
+            data.Close();
+            lectura.Close();
+
+        }
+    }
+}
