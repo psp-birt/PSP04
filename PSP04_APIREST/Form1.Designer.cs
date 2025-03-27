f7266b6c2cc331088978bab4997d7c42138e909c
psp-birt <95621146+psp-birt@users.noreply.github.com>
2025-03-27 21:48:04 +0100

    Add files via upload

---
+++ PSP04_APIREST/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿
+namespace API_REST {
+  partial class Form1 {
+    /// <summary>
+    /// Variable del diseñador necesaria.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Limpiar los recursos que se estén usando.
+    /// </summary>
+    /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+    protected override void Dispose(bool disposing) {
+      if (disposing && (components != null)) {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Código generado por el Diseñador de Windows Forms
+
+    /// <summary>
+    /// Método necesario para admitir el Diseñador. No se puede modificar
+    /// el contenido de este método con el editor de código.
+    /// </summary>
+    private void InitializeComponent() {
+            this.bt_T1 = new System.Windows.Forms.Button();
+            this.bt_T = new System.Windows.Forms.Button();
+            this.lb_temp1 = new System.Windows.Forms.Label();
+            this.txt_consulta = new System.Windows.Forms.TextBox();
+            this.lb_tiempo1 = new System.Windows.Forms.Label();
+            this.lb_temp2 = new System.Windows.Forms.Label();
+            this.lb_tiempo2 = new System.Windows.Forms.Label();
+            this.txt_ciudad1 = new System.Windows.Forms.TextBox();
+            this.txt_ciudad2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // bt_T1
+            // 
+            this.bt_T1.Location = new System.Drawing.Point(37, 38);
+            this.bt_T1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.bt_T1.Name = "bt_T1";
+            this.bt_T1.Size = new System.Drawing.Size(189, 57);
+            this.bt_T1.TabIndex = 0;
+            this.bt_T1.Text = "API REST temperatura1";
+            this.bt_T1.UseVisualStyleBackColor = true;
+            this.bt_T1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // bt_T
+            // 
+            this.bt_T.Location = new System.Drawing.Point(37, 119);
+            this.bt_T.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.bt_T.Name = "bt_T";
+            this.bt_T.Size = new System.Drawing.Size(189, 57);
+            this.bt_T.TabIndex = 1;
+            this.bt_T.Text = "API REST Temperatura2";
+            this.bt_T.UseVisualStyleBackColor = true;
+            this.bt_T.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // lb_temp1
+            // 
+            this.lb_temp1.AutoSize = true;
+            this.lb_temp1.Location = new System.Drawing.Point(470, 59);
+            this.lb_temp1.Name = "lb_temp1";
+            this.lb_temp1.Size = new System.Drawing.Size(20, 15);
+            this.lb_temp1.TabIndex = 2;
+            this.lb_temp1.Text = "ºC";
+            // 
+            // txt_consulta
+            // 
+            this.txt_consulta.Enabled = false;
+            this.txt_consulta.Location = new System.Drawing.Point(82, 212);
+            this.txt_consulta.Multiline = true;
+            this.txt_consulta.Name = "txt_consulta";
+            this.txt_consulta.Size = new System.Drawing.Size(754, 272);
+            this.txt_consulta.TabIndex = 3;
+            // 
+            // lb_tiempo1
+            // 
+            this.lb_tiempo1.AutoSize = true;
+            this.lb_tiempo1.Location = new System.Drawing.Point(606, 59);
+            this.lb_tiempo1.Name = "lb_tiempo1";
+            this.lb_tiempo1.Size = new System.Drawing.Size(134, 15);
+            this.lb_tiempo1.TabIndex = 4;
+            this.lb_tiempo1.Text = "previsión metereológica";
+            // 
+            // lb_temp2
+            // 
+            this.lb_temp2.AutoSize = true;
+            this.lb_temp2.Location = new System.Drawing.Point(470, 140);
+            this.lb_temp2.Name = "lb_temp2";
+            this.lb_temp2.Size = new System.Drawing.Size(20, 15);
+            this.lb_temp2.TabIndex = 5;
+            this.lb_temp2.Text = "ºC";
+            // 
+            // lb_tiempo2
+            // 
+            this.lb_tiempo2.AutoSize = true;
+            this.lb_tiempo2.Location = new System.Drawing.Point(606, 140);
+            this.lb_tiempo2.Name = "lb_tiempo2";
+            this.lb_tiempo2.Size = new System.Drawing.Size(134, 15);
+            this.lb_tiempo2.TabIndex = 6;
+            this.lb_tiempo2.Text = "previsión metereológica";
+            // 
+            // txt_ciudad1
+            // 
+            this.txt_ciudad1.Location = new System.Drawing.Point(250, 56);
+            this.txt_ciudad1.Name = "txt_ciudad1";
+            this.txt_ciudad1.Size = new System.Drawing.Size(154, 23);
+            this.txt_ciudad1.TabIndex = 7;
+            this.txt_ciudad1.Text = "inserta nombre ciudad";
+            // 
+            // txt_ciudad2
+            // 
+            this.txt_ciudad2.Location = new System.Drawing.Point(250, 132);
+            this.txt_ciudad2.Name = "txt_ciudad2";
+            this.txt_ciudad2.Size = new System.Drawing.Size(154, 23);
+            this.txt_ciudad2.TabIndex = 8;
+            this.txt_ciudad2.Text = "inserta nombre ciudad";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(933, 519);
+            this.Controls.Add(this.txt_ciudad2);
+            this.Controls.Add(this.txt_ciudad1);
+            this.Controls.Add(this.lb_tiempo2);
+            this.Controls.Add(this.lb_temp2);
+            this.Controls.Add(this.lb_tiempo1);
+            this.Controls.Add(this.txt_consulta);
+            this.Controls.Add(this.lb_temp1);
+            this.Controls.Add(this.bt_T);
+            this.Controls.Add(this.bt_T1);
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.Button bt_T1;
+    private System.Windows.Forms.Button bt_T;
+        private Label lb_temp1;
+        private TextBox txt_consulta;
+        private Label lb_tiempo1;
+        private Label lb_temp2;
+        private Label lb_tiempo2;
+        private TextBox txt_ciudad1;
+        private TextBox txt_ciudad2;
+    }
+}
+
---
+++ PSP04_APIREST/Form1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Net.Mail;
+using System.Net.Http;
+using System.Text.Json.Nodes;
+using Newtonsoft.Json.Converters;
+using Newtonsoft.Json;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using Newtonsoft.Json.Linq;
+
+namespace API_REST {
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            //Crear vuestra APIkey para comprobar que funciona
+            string apikey = "a285be9e83300f50a8a42ffbd0da297a";
+            string ciudad = txt_ciudad1.Text;
+            string unidades = "metric";
+            string url = $"https://api.openweathermap.org/data/2.5/weather?q=" + ciudad + "&appid=" + apikey + "&units=" + unidades;
+
+            using (HttpClient client = new HttpClient())
+
+            using (HttpResponseMessage response = await client.GetAsync(url))
+            {
+                string datociudad = await response.Content.ReadAsStringAsync();
+                dynamic objDatosCiudad = JValue.Parse(datociudad);
+                this.lb_temp1.Text = objDatosCiudad.main.temp + "ºC";
+                this.lb_tiempo1.Text = objDatosCiudad.weather[0].description ;
+                txt_consulta.Text = datociudad;
+                
+
+            }
+            
+        }
+
+        //Crear vuestra APIkey para comprobar que funciona
+        private async void button2_Click(object sender, EventArgs e)
+        {
+            this.txt_consulta.Clear();
+
+            //crea tus propias credenciales para que funcione.
+            string apikey = "a285be9e83300f50a8a42ffbd0da297a";
+            string ciudad = txt_ciudad2.Text;
+            string unidades = "metric";
+
+            string url = $"https://api.openweathermap.org/data/2.5/weather?q=" + ciudad + "&appid=" + apikey + "&units=" + unidades;
+
+            using (HttpClient client = new HttpClient())
+
+            using (HttpResponseMessage response = await client.GetAsync(url))
+            {
+                string datociudad = await response.Content.ReadAsStringAsync();
+                ResultadoTiempo datosTiempo = JsonConvert.DeserializeObject<ResultadoTiempo>(datociudad);
+                this.lb_temp2.Text = datosTiempo.Main.Temp.ToString() + "ºC";
+                this.lb_tiempo2.Text = datosTiempo.Weather[0].Description ;
+                txt_consulta.Text = datociudad;
+            }
+        }
+    }
+    public class Coord
+    {
+        public double Lon { get; set; }
+        public double Lat { get; set; }
+    }
+
+    public class Weather
+    {
+        public int Id { get; set; }
+        public string Main { get; set; }
+        public string Description { get; set; }
+        public string Icon { get; set; }
+    }
+
+    public class Main
+    {
+        public double Temp { get; set; }
+        public double Feels_like { get; set; }
+        public double Temp_min { get; set; }
+        public double Temp_max { get; set; }
+        public int Pressure { get; set; }
+        public int Humidity { get; set; }
+    }
+
+    public class Wind
+    {
+        public double Speed { get; set; }
+        public int Deg { get; set; }
+    }
+
+    public class Clouds
+    {
+        public int All { get; set; }
+    }
+
+    public class Sys
+    {
+        public int Type { get; set; }
+        public int Id { get; set; }
+        public string Country { get; set; }
+        public int Sunrise { get; set; }
+        public int Sunset { get; set; }
+    }
+
+    public class ResultadoTiempo
+    {
+        public Coord Coord { get; set; }
+        public List<Weather> Weather { get; set; }
+        public string Base { get; set; }
+        public Main Main { get; set; }
+        public int Visibility { get; set; }
+        public Wind Wind { get; set; }
+        public Clouds Clouds { get; set; }
+        public int Dt { get; set; }
+        public Sys Sys { get; set; }
+        public int Timezone { get; set; }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Cod { get; set; }
+    }
+
+}
